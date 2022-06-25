027fc79afa6738d41aa7d0de1c5277b99b5cadd3
MrSzpachla <kamilpaszkiet@gmail.com>
2022-06-25 09:10:59 +0200

    Dodaj pliki projektów.

---
+++ MVC_Lab/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Lab.Models;
+
+namespace MVC_Lab.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new TestModel();
+            model.Imie = "Kamil";
+            model.Nazwisko = "Paszkiet";
+            model.NrAlbum = 49750;
+            return View(model);
+        }
+    }
+}
---
+++ MVC_Lab/Models/TestModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace MVC_Lab.Models
+{
+    public class TestModel
+    {
+        public string? Imie { get; set; }
+        public string? Nazwisko { get; set; }
+        public int? NrAlbum { get; set; }
+    }
+}
